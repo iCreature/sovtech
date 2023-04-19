4abf69e1335a08ee4d1ee492cf0b8686c4789a38
iCreature <50922474+iCreature@users.noreply.github.com>
2023-04-19 23:03:21 +0200

    Done!

---
+++ src/app/full-view/full-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-full-view',
+  templateUrl: './full-view.component.html',
+  styleUrls: ['./full-view.component.scss']
+})
+export class FullViewComponent {
+
+}
